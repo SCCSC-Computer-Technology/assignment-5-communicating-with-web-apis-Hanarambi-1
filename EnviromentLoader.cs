829cf70e98e688d2752cf27d84dc68c8efa8079e
Hanarambi <98725596+Hanarambi@users.noreply.github.com>
2024-04-14 23:21:35 -0400

    Add files via upload

---
+++ EnviromentLoader.cs
@@ -0,0 +1,49 @@
+﻿namespace StudentProfileWebApp
+{
+    public class EnvironmentLoader
+    {
+        private readonly string _filePath;
+
+        
+        /// Initializes a new instance of the <see cref="EnvironmentLoader"/> class
+        
+        public EnvironmentLoader() : this(".env")
+        {
+        }
+
+        
+        /// Initializes a new instance of the <see cref="EnvironmentLoader"/> class
+       
+        public EnvironmentLoader(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+       
+        public bool LoadVariables()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return false;
+            }
+
+            var lines = File.ReadAllLines(_filePath);
+            foreach (var line in lines)
+            {
+                var parts = line.Split('=', 2);
+                if (parts.Length != 2)
+                {
+                    continue; // Skips lines that don't properly split into key and value.
+                }
+
+                var (key, value) = (parts[0], parts[1]);
+                if (!string.IsNullOrWhiteSpace(key) && !string.IsNullOrWhiteSpace(value))
+                {
+                    Environment.SetEnvironmentVariable(key.Trim(), value.Trim());
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.Net.Http.Headers;
+using StudentProfileWebApp;
+using System.Net.Http.Headers;
+
+DotEnvReader reader = new DotEnvReader();
+bool dotEnvOk = reader.LoadEnv();
+if (!dotEnvOk)
+{
+    Console.WriteLine("Env file not found or contains bad syntax. Ignoring...");
+}
+
+var builder = WebApplication.CreateBuilder(args);
+
+string? listenProtocol = Environment.GetEnvironmentVariable("LISTEN_PROTOCOL");
+string? listenAddress = Environment.GetEnvironmentVariable("LISTEN_ADDRESS");
+string? listenPort = Environment.GetEnvironmentVariable("LISTEN_PORT");
+
+listenProtocol ??= "http";
+listenAddress ??= "127.0.0.1";
+listenPort ??= "5000";
+
+Console.WriteLine(string.Format("Listening on {0}:{1} using {2}", listenAddress, listenPort, listenProtocol));
+builder.WebHost.UseUrls(string.Format("{0}://{1}:{2}", listenProtocol, listenAddress, listenPort));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+string? apiBaseUrl = Environment.GetEnvironmentVariable("STUDENT_PROFILE_API_BASE_URL");
+apiBaseUrl ??= "http://127.0.0.1:3000";
+
+builder.Services.AddHttpClient("StudentProfilesWebApi",
+    (options) =>
+    {
+        options.BaseAddress = new Uri(apiBaseUrl);
+        new MediaTypeWithQualityHeaderValue("application/json", 1.0);
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+   https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
